db6eb56e9ca1f7bc2a7c887b21877127cfa72575
Lampros Kaimakamis <lkaimaka@windowslive.com>
2014-11-05 16:28:31 +0200

    ui.r

--- ui.r
+++ ui.r
@@ -18,16 +18,17 @@ shinyUI(pageWithSidebar(
                    Tab='\t'),
                  selected = NULL),
     
-    numericInput("obs", "Number of observations to view:", 100),
+    numericInput(inputId ="obs",label = "Number of observations to view:", 100),
     
-    checkboxInput("plotting", "Plotting",
+    checkboxInput(inputId ="plotting", label ="Plotting",
                   value = FALSE),  
     
     conditionalPanel(  
       condition = "input.plotting == true" ,
       
       
-      selectInput("plot",label="Plot Type",choices=list('None'='none',"Histogram"="hist",'Density Chart'='dens',
+      selectInput(inputId ="plot",label=strong("Plot Type"),
+                  choices=list('None'='none',"Histogram"="hist",'Density Chart'='dens',
                                                         "Linechart"="line","Pointchart"="point")),
       
       conditionalPanel(
@@ -43,17 +44,19 @@ shinyUI(pageWithSidebar(
                     choices=names(dataset)),
         
         
-        sliderInput("bin",label="Binwidth",
+        sliderInput(inputId ="bin",label="Binwidth",
                     min=0,max=200,value=1,step=0.1),
         
-        actionButton("goButton1", strong("Update View", style = "color:blue")),
+        actionButton(inputId ="goButton1", label =strong("Update View", style = "color:blue")),
         
         
-        selectInput("fil", "Fill",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="fil", label ="Fill",
+                    choices=c('None'='.',names(dataset))),
         
-        selectInput('facet_row', 'Facet Row', c('None'='.',names(dataset))),
-        selectInput('facet_col', 'Facet Column', c('None'='.',names(dataset)))
+        selectInput(inputId ='facet_row', label ='Facet Row', 
+                    choices= c('None'='.',names(dataset))),
+        selectInput(inputId ='facet_col', label ='Facet Column', 
+                    choices= c('None'='.',names(dataset)))
         
       ),
       
@@ -62,17 +65,19 @@ shinyUI(pageWithSidebar(
         
         selectInput(inputId = "attributes.dens",
                     label = "ATTRIBUTES",
-                    choices=names(dataset)),
+                    choices= names(dataset)),
         
         
-        actionButton("goButton2", strong("Update View", style = "color:blue")),
+        actionButton(inputId ="goButton2", label =strong("Update View", style = "color:blue")),
         
         
-        selectInput("fil.dens", "Fill",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="fil.dens", label ="Fill",
+                    choices= c('None'='.',names(dataset))),
         
-        selectInput('facet_row.dens', 'Facet Row', c('None'='.',names(dataset))),
-        selectInput('facet_col.dens', 'Facet Column', c('None'='.',names(dataset)))
+        selectInput(inputId ='facet_row.dens', label ='Facet Row', 
+                    choices= c('None'='.',names(dataset))),
+        selectInput(inputId ='facet_col.dens', label ='Facet Column', 
+                    choices= c('None'='.',names(dataset)))
         
       ),
       
@@ -89,16 +94,18 @@ shinyUI(pageWithSidebar(
           label = "Y",
           choices = names(dataset),
         ) ,
-        actionButton("goButton4", strong("Update View", style = "color:blue")),
+        actionButton(inputId ="goButton4", label =strong("Update View", style = "color:blue")),
         
-        selectInput("filline", "Fill",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="filline", label ="Fill",
+                    choices= c('None'='.',names(dataset))),
         
-        selectInput("sizeline", "Size",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="sizeline", label ="Size",
+                    choices= c('None'='.',names(dataset))),
         
-        selectInput('facet_rowline', 'Facet Row', c('None'='.',names(dataset))),
-        selectInput('facet_colline', 'Facet Column', c('None'='.',names(dataset)))
+        selectInput(inputId ='facet_rowline', label ='Facet Row', 
+                    choices=c('None'='.',names(dataset))),
+        selectInput(inputId ='facet_colline', label ='Facet Column', 
+                    choices=c('None'='.',names(dataset)))
         
       ),
       
@@ -115,23 +122,25 @@ shinyUI(pageWithSidebar(
           label = "Y",
           choices = names(dataset),
         ),
-        actionButton("goButton5", strong("Update View", style = "color:blue")),
+        actionButton(inputId ="goButton5", label =strong("Update View", style = "color:blue")),
         
-        selectInput("filpoint", "Fill",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="filpoint", label ="Fill",
+                    choices=c('None'='.',names(dataset))),
         
-        selectInput("sizepoint", "Size",choices=
-                      c('None'='.',names(dataset))),
+        selectInput(inputId ="sizepoint", label ="Size",
+                    choices=c('None'='.',names(dataset))),
         
-        selectInput('facet_rowpoint', 'Facet Row', c('None'='.',names(dataset))),
-        selectInput('facet_colpoint', 'Facet Column', c('None'='.',names(dataset)))
+        selectInput(inputId ='facet_rowpoint', label ='Facet Row', 
+                    choices= c('None'='.',names(dataset))),
+        selectInput(inputId ='facet_colpoint', label ='Facet Column', 
+                    choices= c('None'='.',names(dataset)))
         
       )                
       
       
     ),
     
-    checkboxInput("reg", "Modelling",
+    checkboxInput(inputId ="reg", label ="Modelling",
                   value = FALSE),  
     
     tags$hr(),
@@ -166,28 +175,28 @@ shinyUI(pageWithSidebar(
   p(strong("Model predictions")),
   
   
-  checkboxInput(inputId = "mod_linear",    label = "Linear (two-dash)"),
+  checkboxInput(inputId = "mod_linear", label = "Linear (two-dash)"),
   conditionalPanel(
     condition = "input.mod_linear == true",
     checkboxInput(inputId ="stder",label = "Std Error")
   ), 
   
-  checkboxInput(inputId = "mod_rlm",    label = "Robust Linear (F1)"),
+  checkboxInput(inputId = "mod_rlm", label = "Robust Linear (F1)"),
   conditionalPanel(
     condition = "input.mod_rlm == true",
-    checkboxInput(inputId ="stder.rlm",label = "Std Error")
+    checkboxInput(inputId ="stder.rlm", label = "Std Error")
   ), 
   
   checkboxInput(inputId = "mod_quadratic", label = "Quadratic (dashed)"),
   conditionalPanel(
     condition = "input.mod_quadratic == true",
-    checkboxInput(inputId ="stder.quad",label = "Std Error")
+    checkboxInput(inputId ="stder.quad", label = "Std Error")
   ),
   
-  checkboxInput(inputId = "mod_loess",     label = "Locally weighted LOESS (solid)"),
+  checkboxInput(inputId = "mod_loess",  label = "Locally weighted LOESS (solid)"),
   conditionalPanel(
     condition = "input.mod_loess == true",
-    checkboxInput(inputId ="stder.loess",label = "Std Error"),
+    checkboxInput(inputId ="stder.loess", label = "Std Error"),
  
     sliderInput(inputId = "mod_loess_span", label = "Smoothing (alpha)",
                 min = 0.15, max = 1, step = 0.05, value = 0.75)
@@ -214,15 +223,60 @@ shinyUI(pageWithSidebar(
   conditionalPanel(
     condition = "input.radio != 'All'",
     checkboxGroupInput(
-      "mult_reg_x", 
-      "Choose Predictor variables",
+      inputId ="mult_reg_x", 
+      label = "Choose Predictor variables",
       choices=names(dataset) 
       
     )
   ),
- actionButton("goButtonmod", strong("Update Model", style = "color:blue"))
+ actionButton(inputId ="goButtonmod", label =strong("Update Model", style = "color:blue"))
   
-  )
+  ),
+ 
+ tags$hr(),
+ 
+ checkboxInput(inputId = "glm", label = strong("GLM")),
+ conditionalPanel(
+   condition = "input.glm == true",
+   selectInput(inputId = "glm_y",
+               label = strong("Response variable"),
+               choices = names(dataset)
+   ),
+   radioButtons(
+     inputId="radioglm",
+     label=strong("Predictor variable Selection Type:"),
+     choices=list(
+       "All",
+       "Manual Select"
+     ),
+     selected="Manual Select"),
+   
+   conditionalPanel(
+     condition = "input.radioglm != 'All'",
+     checkboxGroupInput(
+       inputId = "glm_x", 
+       label="Choose Predictor variables",
+       choices=names(dataset) 
+       
+     )
+   ),
+   radioButtons(
+     inputId="family", 
+     label= strong("Choose Family"),
+     choices=c('Gaussian'='gauss',
+               'Poisson'='pois',
+               'Binomial'='binom',
+               'Negative Binomial'='neg.bin',
+               'Gamma'='gamma'),
+     selected="gauss"
+     
+   ),
+   
+   actionButton(inputId ="goButtonglm", label =strong("Update Model", style = "color:blue"))
+   
+ )
+ 
+ 
   )),
 
  
@@ -247,7 +301,8 @@ shinyUI(pageWithSidebar(
                  conditionalPanel("input.mod_loess == true",p(strong("LOESS model")),
                                   verbatimTextOutput(outputId = "mod_loess_text"))
                   ),
-         tabPanel("Multiple Regression",verbatimTextOutput(outputId = "mult_reg_text"))
+         tabPanel("Multiple Regression",verbatimTextOutput(outputId = "mult_reg_text")),
+         tabPanel("GLM",verbatimTextOutput(outputId = "glm_text"))
         
         
         )
