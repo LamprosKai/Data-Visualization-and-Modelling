695e7a1efda5183537d7e16d3dbba1783783f332
Lampros Kaimakamis <lkaimaka@windowslive.com>
2014-11-12 20:03:51 +0200

    server.r

--- server.r
+++ server.r
@@ -1,6 +1,9 @@
 
 library(ggplot2)
 library(MASS)
+library(car)
+library(coefplot)
+
 
 shinyServer(function(input, output,session) {
   
@@ -22,13 +25,16 @@ shinyServer(function(input, output,session) {
   read.csv(inFile$datapath, header=input$header, sep=input$sep)       #, quote=input$quote
  
   })  
-   
+  
  observe({
  df <- data()
 
  if (!is.null(df)) {
    updateSelectInput(session, 'attributes', choices = names(df))
    updateSelectInput(session, 'attributes.dens', choices = names(df))
+   updateSelectInput(session, 'attributes.bar', choices = names(df))
+   updateSelectInput(session, 'attributes.ident', choices = names(df))
+   
    
    updateSelectInput(session, 'attributx', choices = names(df))
    updateSelectInput(session, 'attributy', choices = names(df))
@@ -43,6 +49,7 @@ shinyServer(function(input, output,session) {
    updateSelectInput(session, 'fil.dens', choices = c(None='.', names(df)))
    updateSelectInput(session, 'filline', choices = c(None='.', names(df)))
    updateSelectInput(session, 'filpoint', choices = c(None='.', names(df)))
+   updateSelectInput(session, 'filbar', choices = c(None='.', names(df)))
    
    
    updateSelectInput(session, 'facet_col', choices = c(None='.', names(df)))
@@ -53,6 +60,8 @@ shinyServer(function(input, output,session) {
    updateSelectInput(session, 'facet_colpoint', choices = c(None='.', names(df)))
    updateSelectInput(session, 'facet_col.dens', choices = c(None='.', names(df)))
    updateSelectInput(session, 'facet_row.dens', choices = c(None='.', names(df)))
+   updateSelectInput(session, 'facet_col.bar', choices = c(None='.', names(df)))
+   updateSelectInput(session, 'facet_row.bar', choices = c(None='.', names(df)))
    
    updateSelectInput(session, 'reg_y', choices = c(None='.', names(df)))
    updateSelectInput(session, 'reg_x', choices = c(None='.', names(df)))
@@ -65,6 +74,16 @@ shinyServer(function(input, output,session) {
                                                          Binomial='binom',
                                                    Negative.Binomial='neg.bin',
                                                    Gamma='gamma'))
+   updateSelectInput(session, 'radio.plots', choices = c(None='.', Added_Variable_Plot="avplot",
+                                                         Residual_Plot="resplot",
+                                                         Marginal_Plot="margplot",
+                                                         Partial_Residual_Plot="crplot"))
+   updateSelectInput(session, 'radio.plots.glm', choices = c(None='.', Added_Variable_Plot="avplot",
+                                                         Residual_Plot="resplot",
+                                                         Marginal_Plot="margplot",
+                                                         Partial_Residual_Plot="crplot",
+                                                         Coefplot='coefplot'))
+   
  }
 }) 
  
@@ -76,6 +95,7 @@ output$Plot <- renderPlot({
   input$goButton
   input$goButton1
   input$goButton2
+  input$goButton3
   input$goButton4
   input$goButton5
   
@@ -89,7 +109,7 @@ output$Plot <- renderPlot({
     if ( input$plot == "hist" ) {
       
       if(input$fil!="." ){
-        aes_mapping <- aes_string(x = input$attributes,group=input$fil,fill=input$fil) } #
+        aes_mapping <- aes_string(x = input$attributes,group=input$fil,fill=input$fil) }
       
      
       else{
@@ -109,12 +129,53 @@ output$Plot <- renderPlot({
     
   }) 
   
+  
+  isolate({
+    
+    if ( input$plot == "bar" ) {
+      
+      if(input$filbar!="." ){
+        if (input$identity == TRUE ) {
+          aes_mapping <- aes_string(x = input$attributes.bar, y=input$attributes.ident ,group=input$filbar,fill=input$filbar)}
+          else {
+          aes_mapping <- aes_string(x = input$attributes.bar,group=input$filbar,fill=input$filbar) }
+      }
+      else if(input$identity == TRUE){
+        aes_mapping <- aes_string(x = input$attributes.bar, y=input$attributes.ident)
+      }
+          else {
+            
+            aes_mapping <- aes_string(x =input$attributes.bar)  }
+        
+        
+    
+      
+      
+      #plot
+      p <- ggplot(data(),mapping=aes_mapping )
+     
+     if (input$identity ==TRUE ){
+       p<- p+geom_bar(stat='identity') }
+      
+      else{
+        p<- p+ geom_bar()
+      }
+      
+      facets <- paste(input$facet_row.bar, '~', input$facet_col.bar)
+      if (facets != '. ~ .')
+        p <- p + facet_grid(facets,scales='free')  +theme(axis.text.x=element_text(angle = 90, hjust = 1,vjust=0.4,size=9))
+      
+    }
+    
+  }) 
+  
+  
   isolate({
     
     if ( input$plot == "dens" ) {
       
       if(input$fil.dens!="." ){
-        aes_mapping <- aes_string(x = input$attributes.dens,group=input$fil.dens,fill=input$fil.dens) } #
+        aes_mapping <- aes_string(x = input$attributes.dens,group=input$fil.dens,fill=input$fil.dens) } 
       
       
       else{
@@ -140,7 +201,7 @@ output$Plot <- renderPlot({
     
     if(input$filline!='.' ){
       if(input$sizeline!='.'){
-        aes_mapping <- aes_string(x =input$attributx,y=input$attributy,color=input$filline,size=input$sizeline) }       #
+        aes_mapping <- aes_string(x =input$attributx,y=input$attributy,color=input$filline,size=input$sizeline) }       
       else{
         aes_mapping <- aes_string(x =input$attributx,y=input$attributy,color=input$filline) }
     }
@@ -185,6 +246,14 @@ output$Plot <- renderPlot({
   
   print(p)
 })
+
+output$downloadPlot <- downloadHandler(
+  filename = function() { paste(input$file1, '.png', sep='') },
+  content = function(file) {
+    device <- function(..., width, height) grDevices::png(..., width = width, height = height, res = 300, units = "in")
+    ggsave(file,  device = device)
+  }
+)
   
   output$Regresion <- renderPlot({
    
@@ -202,7 +271,7 @@ output$Plot <- renderPlot({
      
   
     # Base plot
-    p <- ggplot(data(), mapping = aes_string(x =input$reg_x,y=input$reg_y)) + #,fill=input$reg_x,group=1
+    p <- ggplot(data(), mapping = aes_string(x =input$reg_x,y=input$reg_y)) + 
       geom_point(shape = 21, alpha = point_alpha)+
       scale_colour_hue(l = 40) +
       scale_shape(solid = FALSE) 
@@ -248,8 +317,80 @@ output$Plot <- renderPlot({
     
     print(p)
   })
+
+output$Mult_Regresion <- renderPlot({
   
   
+  input$goButtonmod
+  if(input$goButtonmod > 0){
+    
+    isolate({
+      if(input$radio == "All"){
+        formula <- paste(input$mult_reg_y, "~.")
+        
+      }
+      
+      else{
+        
+        formula <- paste(input$mult_reg_y, "~", paste(input$mult_reg_x,collapse='+'))
+      }
+    })
+    
+  }
+  
+  if(input$radio.plots == "avplot"){
+    avPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots == "resplot"){
+    residualPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots == "margplot"){
+    marginalModelPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots == "crplot"){
+    crPlots(lm(formula,data=data()))
+  }
+  
+})
+
+output$GLM_Regresion <- renderPlot({
+  
+  
+  input$goButtonglm
+  if(input$goButtonglm > 0){
+    
+    isolate({
+      if(input$radioglm == "All"){
+        formula <- paste(input$glm_y, "~.")
+        
+      }
+      
+      else{
+        
+        formula <- paste(input$glm_y, "~", paste(input$glm_x,collapse='+'))
+      }
+    })
+    
+  }
+  
+  if(input$radio.plots.glm == "avplot"){
+    avPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots.glm == "resplot"){
+    residualPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots.glm == "margplot"){
+    marginalModelPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots.glm == "crplot"){
+    crPlots(lm(formula,data=data()))
+  }
+  if(input$radio.plots.glm == "coefplot"){
+    coefplot(lm(formula,data=data()))
+  }
+  
+})
+
   # ------------------------------------------------------------------
  # Generate a summary of the dataset
    output$summary <- renderPrint({
@@ -268,21 +409,24 @@ output$Plot <- renderPlot({
     
     # In order to get the output to print the formula in a nice way, we'll
     # use do.call here with some quoting.
-    if(!input$glm){
-    do.call(model_type, args = list(formula = formula,data = quote(data()),  ...)) }
+   if(!input$glm){
+     do.call(model_type, args = list(formula = formula,data = quote(data()),  ...)) }
     
     else if(input$family=='pois'){
-      do.call(model_type, args = list(formula = formula,data = quote(data()),family=poisson(),  ...))
+      do.call(model_type, args = list(formula = formula,data = quote(data()),family=quote(poisson),  ...))
     }
     else if(input$family=='binom'){
-      do.call(model_type, args = list(formula = formula,data = quote(data()),family=binomial(),  ...))
+      do.call(model_type, args = list(formula = formula,data = quote(data()),family=quote(binomial),  ...))
     }
-    else if(input$family=='gamma'){
-      do.call(model_type, args = list(formula = formula,data = quote(data()),family=Gamma(link='log'),  ...))
+    else if (input$family=='gamma'  && any(input$glm_y <= 0)){
+      do.call(model_type, args = list(formula = formula,data = quote(data()),family=quote(Gamma),subset=input$glm_y >0,  ...))
     }
+    
+   
     else{
       do.call(model_type, args = list(formula = formula,data = quote(data()),  ...))
     }
+    
   }
 
 
@@ -333,7 +477,8 @@ output$Plot <- renderPlot({
   summary(make_model("lm", formula)) }
     })
   
-} else{
+} 
+else{
   return(cat('Must choose one or more predictor variables and hit Update Model \n'))
 }
 
@@ -355,7 +500,7 @@ output$Plot <- renderPlot({
       
       else if(input$radioglm != "All" && input$family != 'neg.bin'){
         
-        formula <- paste(input$glm_y, "~", paste(input$glm_x,collapse='+')) #(link="'log'")
+        formula <- paste(input$glm_y, "~", paste(input$glm_x,collapse='+')) 
         summary(make_model("glm", formula)) }
       
       else {
@@ -371,6 +516,43 @@ output$Plot <- renderPlot({
   
 })
 
+output$help_text <- renderText({ 
   
+
+'Data Visualization and Modelling application using an uploaded csv.
+
+ Part of the data and data summary is shown immediately after the uploading data set(by clicking on the specified tabs).
+ The user is able to choose between plotting and modelling by "clicking" the boxes.
+ 
+ Plotting consists of histogram, barplot, density plot, line plot and point plot with the associated colour, size and faceting capabilities.
+ For example when histogram is chosen , x axis is requested and then there is options for filling and faceting. The user chooses the 
+ appropriate data columns.
+ Barplot has one more option that of "identity", when the users "click" the box,the heights of the bars represent values in the data, and map a 
+ value to the y aesthetic
+ The same exist with line and point plots with size option(the size of point or line) as an addition.
+
+ Modelling consists of Simple regression, Multiple regression and Generalized Linear Model(GLM). By "clicking" the respective box
+ more options are available. 
+
+ Simple Regression consists of one response and one dependent variable in the form "y~x" and the user chooses the appropriate variables.
+ A plot of the selected raw columns appeared. By choosing one or more of the following boxes(Linear, Robust Linear) the app presents the 
+ respective model summary and the smooth model line on the plot. Std.Error depicts the 95% confidence interval.
+ 
+ Multiple regression builds a linear model of the form y~x1+x2+...+xk by choosing manual the number of predictor variables or choosing all variables.
+ In Addition there are options of plotting some diagnostic plots. These are:
+ Added variable plot: Attempts to show the effect of adding another variable to a model already having one or more independent variables.
+ Residual plot: Plots the residuals versus each term in a mean function and versus fitted values. Also computes a curvature test for each of the plots 
+ by adding a quadratic term and testing the quadratic to be zero.
+ Marginal model plot:  plots the response on the vertical axis versus a linear combination of terms in the mean function on the horizontal axis. 
+ Added to the plot are a loess smooth for the graph, along with a loess smooth from the plot of the fitted values.
+ Partial residual plot: Attempts to show the relationship between the given independent variable and the response variables given that the other 
+ independent variables are also in the model.
+ 
+ GLM builds a generalized linear model, again by choosing manual the number of predictor variables or choosing all variables, in addition to 
+ family selection of the error term. 
+ There are the same diagnostic plots as in multiple regression plus a coefplot, that is the coefficients and standard errors from a fitted model.
+
+ ' 
+             
   
 })
